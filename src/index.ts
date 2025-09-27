637208e164f4939a29e7a17ce9c3ae4d8dd4c3d2
Florian <florian.derue@student.vinci.be>
2025-09-27 14:59:39 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello TypeScript + Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
+export default app;
